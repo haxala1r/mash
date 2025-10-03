561c76b6d779ecf4880046fff118b803513264fc
haxala1r <eminarslan@emin.software>
2025-10-03 22:06:10 +0300

    fix tests to obey new rules

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -6,16 +6,16 @@ TEST_CASE("Lexer lexes doubles correctly", "[Lexer]") {
     
     SECTION("double and negative syntax") {
         Lexer l("(1.0 0.1 -.1 -1. . - -. .-)");
-        REQUIRE(l.next() == Token({OpenParen, nullopt}));
-        REQUIRE(l.next() == Token({Double, 1.0}));
-        REQUIRE(l.next() == Token({Double, 0.1}));
-        REQUIRE(l.next() == Token({Double, -0.1}));
-        REQUIRE(l.next() == Token({Double, -1.0}));
-        REQUIRE(l.next() == Token({Symbol, "."}));
-        REQUIRE(l.next() == Token({Symbol, "-"}));
-        REQUIRE(l.next() == Token({Symbol, "-."}));
-        REQUIRE(l.next() == Token({Symbol, ".-"}));
-        REQUIRE(l.next() == Token({CloseParen, nullopt}));
+        REQUIRE(l.next() == Token({TokenType::OpenParen, nullopt}));
+        REQUIRE(l.next() == Token({TokenType::Double, 1.0}));
+        REQUIRE(l.next() == Token({TokenType::Double, 0.1}));
+        REQUIRE(l.next() == Token({TokenType::Double, -0.1}));
+        REQUIRE(l.next() == Token({TokenType::Double, -1.0}));
+        REQUIRE(l.next() == Token({TokenType::Symbol, "."}));
+        REQUIRE(l.next() == Token({TokenType::Symbol, "-"}));
+        REQUIRE(l.next() == Token({TokenType::Symbol, "-."}));
+        REQUIRE(l.next() == Token({TokenType::Symbol, ".-"}));
+        REQUIRE(l.next() == Token({TokenType::CloseParen, nullopt}));
     }
 }
 
